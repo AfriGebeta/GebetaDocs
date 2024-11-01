3985078d031382042c9723f66f1f422c10d4413a
zubeyranwar <zubeyranwar@gmail.com>
2024-11-01 10:31:48 +0300

    feat: Implement Footer section

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,11 +1,19 @@
+"use client"
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/home-layout';
 import { baseOptions } from '../layout.config';
+import StoreProvider from "@/poviders/StoreProvider";
 
 export default function Layout({
   children,
 }: {
   children: ReactNode;
 }): React.ReactElement {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <StoreProvider>
+        <HomeLayout {...baseOptions}>
+            {children}
+        </HomeLayout>s
+    </StoreProvider>
+      )
 }
