51091c1896226db8d8b2b34e0773c8c12c9070af
zubeyranwar <zubeyranwar@gmail.com>
2025-01-20 15:01:19 +0300

    feat: Show waypoints in optional parameter

--- data/playground.js
+++ data/playground.js
@@ -17,7 +17,7 @@ const DirectionObject = {
     requestSampleUrl : "http://localhost:8080/api/route/direction/?origin={8.987685259188599,38.764792722654455}&destination={9.087685259188599,38.764792722654455}&traffic=0&instruction=0&apiKey=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb21wYW55bmFtZSI6ImdlYmV0YSIsImRlc2NyaXB0aW9uIjoibnVsbCBzdHJpbmcgIiwiaWQiOiJhMGFiYzRkYy0yNjRhLTRjNGYtYjU3MC0yYWU0ODhmYTViYzkiLCJ1c2VybmFtZSI6ImdlYmV0YSJ9.pcfDBhYhjIbfBB278X0Ewi7ALeld90F_Vl_JdwjX5qA",
     optionalParameter : [
      //  { name : "instruction"},
-    //    { name : "waypoints"},
+       { name : "waypoints"},
        
     ]
 }
