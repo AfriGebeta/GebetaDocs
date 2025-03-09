419ed127bf1c04413ada4ea20f152531ebfc45a8
Bereket Terefe <bereket.terefe@tabiya.org>
2025-03-09 16:38:06 +0300

    [ feat ] - improve tile documentation - add ci cd that deploys to azure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -58,7 +58,7 @@ export default function HomePage() {
       description:
         "A tile API provides map tiles for displaying maps in your application.",
       icon: <Map />,
-      link: "/docs/tiles/_tiles",
+      link: "/docs/tiles",
     }
   ];
 
