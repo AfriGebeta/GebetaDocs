f65b2d415c23223dd790408e7a4260832fa77e34
Bereket Terefe <bereket.terefe@tabiya.org>
2025-03-09 16:38:06 +0300

    [ feat ] - improve tile documentation - add ci cd that deploys to azure

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-import {createMDX} from 'fumadocs-mdx/next';
-
-const withMDX = createMDX();
+import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true,
-};
+const nextConfig = {
+    output: process.env.NEXT_OUTPUT || 'standalone',
+    reactStrictMode: false,
+    basePath: '',
+    images: {
+        unoptimized: true,
+    },
+    experimental: {
+        optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
+    },
+    // Configure `pageExtensions` to include markdown and MDX files
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
 
-export default withMDX(config);
+export default nextConfig
