54bed9836197c5b9866f84d81bea99d2ac043712
zubeyranwar <zubeyranwar@gmail.com>
2025-04-03 19:47:46 +0300

    feat: Add state to control whether instruction display or not

--- app/(home)/map-playground/MapView.jsx
+++ app/(home)/map-playground/MapView.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import Map from "./Map";
 import SideBarForm from "./SideBarForm";
 import {returnPlaygroundObject} from "data/playground"
@@ -7,7 +7,9 @@ import {useSelector} from "react-redux"
 
 const MapView = () => {
 
-
+    const [showInstructions, setShowInstructions] = useState(false)
+    const [instructions, setInstructions] = React.useState([]);
+    const [activeInstruction, setActiveInstruction] = React.useState(null);
 
     const [selectedButton, setSelectedButton] = useState("");
     const { playground } = useSelector((state) => state)
@@ -22,22 +24,37 @@ const MapView = () => {
     }
 
 
-
+    const t = returnPlaygroundObject(playground.current)
+    useEffect(() => {
+        if(t.type !== "direction") {
+            setInstructions([])
+            setShowInstructions(false)
+        }
+    }, [playground.current]);
 
 
     return (
-        <main className="flex flex-col lg:flex-row gap-[40px] min-h-screen overflow-hidden">
-            <div className="w-full lg:w-1/4 overflow-y-auto">
+        <main className="flex flex-col lg:flex-row gap-[40px] min-h-screen overflow-x-hidden">
+            <div className="w-full lg:w-1/4 overflow-x-hidden">
                 <SideBarForm
                     setSelectedButtonFunction={setSelectedButtonFunction}
                     selectedButton={selectedButton}
                     object={returnPlaygroundObject(playground.current)}
+                    setInstructions={setInstructions}
+                    setActiveInstruction={setActiveInstruction}
+                    setShowInstructions={setShowInstructions}
+                    showInstructions={showInstructions}
                 />
             </div>
 
             <div className="w-full lg:w-3/4 h-screen mx-[2%] md:mx-[0%]">
                 <Map
                     selectedButton = {selectedButton}
+                    instructions={instructions}
+                    activeInstruction={activeInstruction}
+                    setActiveInstruction={setActiveInstruction}
+                    setShowInstructions={setShowInstructions}
+                    showInstructions={showInstructions}
                 />
             </div>
         </main>
