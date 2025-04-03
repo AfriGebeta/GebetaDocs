b5753ec058bc90dbe686b822ce5e3d795cf30962
zubeyranwar <zubeyranwar@gmail.com>
2025-04-03 19:50:35 +0300

    feat: Refactor

--- app/(home)/map-playground/Notify.jsx
+++ app/(home)/map-playground/Notify.jsx
@@ -5,18 +5,19 @@ import React, {useEffect, useState} from "react";
 
 
 function Notify({value}) {
-    const [show,setShow] = useState('hidden');
+    const [show, setShow] = useState('hidden');
 
     useEffect(() => {
 
-        setShow(value.visible?'block':'hidden')
-        setTimeout(() => setShow('hidden'),value.timeout || 2000);
-    },[value.visible])
+        setShow(value.visible ? 'block' : 'hidden')
+        setTimeout(() => setShow('hidden'), value.timeout || 2000);
+    }, [value.visible])
 
 
     return (
-        <div className={" fixed top-[8%] right-[50%] left-[50%] text-center ml-auto mr-auto py-3 text-white "+show}>
-            <div className="w-fit flex items-center gap-2 bg-[#202022] card rounded-3xl px-10 py-2 inline-block shadow-xl shadow-black text-white">
+        <div className={" fixed top-[8%] right-[50%] left-[50%] text-center ml-auto mr-auto py-3 text-white " + show}>
+            <div
+                className={`w-fit  items-center gap-2 ${value.type == "failure" ? "bg-[#ff3333]" : "bg-green-400"} card rounded-3xl px-10 py-2 inline-block shadow-xl text-white`}>
                 {/*{value.type === "success" ? <FaCircleCheck className='text-green-400'/> : <FaCircleXmark className='text-red-700'/>}*/}
                 <p className='whitespace-nowrap'>{value.msg}</p>
             </div>
